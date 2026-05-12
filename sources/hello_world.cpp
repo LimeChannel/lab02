1b1fbda1b37380a81cb0a9e01e0b598f4f5d056a
Lime Channel <limechann@yandex.ru>
2026-05-12 12:40:13 +0300

    added comments to hello_world

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -4,10 +4,10 @@
 int main() {
     std::string name;
 
-    std::cout << "Введите имя: ";
+    std::cout << "Введите имя: "; //! Вот тут вводим имя
     std::getline(std::cin, name);
 
-    std::cout << "Hello world from " << name << std::endl;
+    std::cout << "Hello world from " << name << std::endl; //? А это что такое
 
     return 0;
 }
